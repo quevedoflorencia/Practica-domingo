e63cdf23cda4eb90bb044fa8b79f8558f8b71045
virginiaprats <v.prats@hotmail.com.ar>
2024-07-24 18:34:19 -0300

    se desacoplan los test del controladorCalculadora

--- src/test/java/com/tallerwebi/presentacion/ControladorCalculadoraTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorCalculadoraTest.java
@@ -38,47 +38,11 @@ public class ControladorCalculadoraTest {
     }
 
     @Test
-    public void queAlSolicitarUnCalculoArrojeElResultadoCorrecto(){
+    public void queLuegoDePresionarCalcularMeDevuelvaLaVistaCalculadora(){
         // preparación
-        // double resultadoCalculo = 0.0;
-        // double numeroUno = 3.0;
-        // double numeroDos = 3.0;
-        // String operacion = "+";
-
-        /*Para la lista de historial*/
-        List<CalculadoraData> calculosMock = new ArrayList<>();
-        calculosMock.add(new CalculadoraData(2.0, 4.0,"+"));
-        calculosMock.add(new CalculadoraData(5.0, 4.0,"+"));
-        calculosMock.add(new CalculadoraData(3.0, 3.0,"+"));
-        calculosMock.add(new CalculadoraData(7.0, 3.0,"+"));
-
-
-        //when(this.servicioCalculadora.calcular(6.0, 3.0, "+")).thenReturn(9.0);
-
-        /*Para la lista de historial*/
-        /*when(this.servicioCalculadora.calcular(2.0, 4.0, "+")).thenReturn(6.0);
-        when(this.servicioCalculadora.calcular(5.0, 4.0, "+")).thenReturn(9.0);
-        when(this.servicioCalculadora.calcular(3.0, 3.0, "+")).thenReturn(6.0);*/
-
+        when(this.servicioCalculadora.calcular(6.0, 3.0, "+")).thenReturn(9.0);
         CalculadoraData calculo = new CalculadoraData(6.0, 3.0, "+", 9.0);
-
-        when(this.servicioCalculadora.obtenerHistorial()).thenReturn(calculosMock);
-
-        /*Para la lista de historial*/
-        //CalculadoraData calculoDos = new CalculadoraData(6.0, 3.0, "*", 18.0);
-        //CalculadoraData calculoTres = new CalculadoraData(6.0, 3.0, "*", 18.0);
-       // CalculadoraData calculoCuatro = new CalculadoraData(6.0, 3.0, "*", 18.0);
-
-        //CalculadoraData calculoDos = new CalculadoraData(2.0, 5.0, "*", 10.0);
-
-        //doNothing().when(this.servicioCalculadora).guardarCalculo(calculo); //doNothing porque guardarCalculo es void.
-
-        /*Para la lista de historial*/
-        /*doNothing().when(this.servicioCalculadora).guardarCalculo(calculoDos);
-        doNothing().when(this.servicioCalculadora).guardarCalculo(calculoTres);
-        doNothing().when(this.servicioCalculadora).guardarCalculo(calculoCuatro);*/
-
-
+        doNothing().when(this.servicioCalculadora).guardarCalculo(calculo); //doNothing porque guardarCalculo es void.
 
 
         // ejecución
@@ -86,19 +50,50 @@ public class ControladorCalculadoraTest {
 
 
         //verificación
-
-        //Testeo que el resultado sea correcto
-        //assertThat(mav.getModel().get("resultadoFinal"), equalTo(9.0)); //
-
-
-        //Testeo que el historial tenga la cantidad de calculos correcto
-        //List<CalculadoraData> calculos = (List<CalculadoraData>) mav.getModel().get("historial");
-        //assertThat(calculos.size(), equalTo(4)); // Existan 4 elemento
-
-
-        //Testeo que devuelva la vista correcta que es "Calculadora"
-        assertThat(mav.getViewName(), equalToIgnoringCase("calculadora")); // Vista correcta
+        assertThat(mav.getViewName(), equalToIgnoringCase("calculadora")); // Testeo que devuelva la vista correcta que es "Calculadora"
 
     }
+    @Test
+    public void queLuegoDeCalcularMuestreElHistorialConLaCantidadCorrectaDeCalculos(){
+        //preparacion
+        List<CalculadoraData> calculosMock = new ArrayList<>();
+
+        calculosMock.add(new CalculadoraData(2.0, 4.0,"+"));
+        calculosMock.add(new CalculadoraData(5.0, 4.0,"+"));
+        calculosMock.add(new CalculadoraData(3.0, 3.0,"+"));
+        calculosMock.add(new CalculadoraData(7.0, 3.0,"+"));
+
+        when(this.servicioCalculadora.obtenerHistorial()).thenReturn(calculosMock);
+        CalculadoraData calculo = new CalculadoraData(6.0, 3.0, "+", 9.0);
+
+        //ejecucion
+        ModelAndView mav = this.controladorCalculadora.procesarFormulario(calculo);
+
+        //verificacion
+        //Testeo que el historial tenga la cantidad de calculos correcto
+
+        List<CalculadoraData> calculos = (List<CalculadoraData>) mav.getModel().get("historial");
+        assertThat(calculos.size(), equalTo(4)); // Existan 4 elemento
+
+    }
+
+
+    @Test
+    public void queAlPresionarCalcularArrojeElResultadoCorrectoDeLaOperacion(){
+        //preparacion
+        when(this.servicioCalculadora.calcular(6.0, 3.0, "+")).thenReturn(9.0);
+        CalculadoraData calculo = new CalculadoraData(6.0, 3.0, "+", 9.0);
+        doNothing().when(this.servicioCalculadora).guardarCalculo(calculo); //doNothing porque guardarCalculo es void.
+
+
+        // ejecución
+        ModelAndView mav = this.controladorCalculadora.procesarFormulario(calculo);
+
+        //verificacion
+        //Testeo que el resultado sea correcto
+        assertThat(mav.getModel().get("resultadoFinal"), equalTo(9.0));
+    }
+
+
 
 }

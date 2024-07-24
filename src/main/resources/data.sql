6337ab2aca4b70858f894bdcc0adfbc07cc8769e
Florencia Quevedo <mquevedovaldez@alumno.unlam.edu.ar>
2024-07-24 01:28:15 -0300

    se agregan cálculos en data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
 INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
+
+INSERT INTO CalculadoraData (fecha, operando1, operando2, operador, resultado) VALUES ('2024-07-01', 55, 3,'+', 58 );
+INSERT INTO CalculadoraData (fecha, operando1, operando2, operador, resultado) VALUES ('2024-07-22', 24, 24,'+', 48 );
+INSERT INTO CalculadoraData (fecha, operando1, operando2, operador, resultado) VALUES ('2024-07-20', 500, 200,'-', 300 );
